3e2ab090cd8553a7cab0495e8ba3352eee3de775
Valentin Uveges <valentin.uveges@gmail.com>
2017-08-25 09:20:10 +0300

    Update install.sh

--- system/install.sh
+++ system/install.sh
@@ -7,3 +7,8 @@ yum groupinstall 'Development Tools'
 #configure network
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 systemctl restart network.service
+
+#install haveged for tomcat start issues and entropy
+yum install -y haveged
+systemctl enable haveged
+systemctl start haveged
