b7792036b8fe83ffe234afe90573fc49a9e98cb8
Valentin Uveges <valentin.uveges@gmail.com>
2017-08-25 09:30:44 +0300

    Update install.sh

--- maven/install.sh
+++ maven/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-wget "http://mirrors.m247.ro/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
-tar -xzf apache-maven-3.5.0-bin.tar.gz
-mv apache-maven-3.5.0-bin.tar.gz /opt/maven
+
+MAVEN_VERSION=apache-maven-3.5.0
+
+wget "http://mirrors.m247.ro/apache/maven/maven-3/3.5.0/binaries/$MAVEN_VERSION-bin.tar.gz"
+tar -xzf $MAVEN_VERSION-bin.tar.gz
+mv $MAVEN_VERSION-bin /opt/maven
 
 cp maven.sh /etc/profile.d/
 source /etc/profile.d/
