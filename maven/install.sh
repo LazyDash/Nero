327be6f017690c892feaa37834c4b4a3963c9001
Valentin Uveges <valentin.uveges@gmail.com>
2017-08-25 09:48:08 +0300

    Update install.sh

--- maven/install.sh
+++ maven/install.sh
@@ -4,7 +4,9 @@ MAVEN_VERSION=apache-maven-3.5.0
 
 wget "http://mirrors.m247.ro/apache/maven/maven-3/3.5.0/binaries/$MAVEN_VERSION-bin.tar.gz"
 tar -xzf $MAVEN_VERSION-bin.tar.gz
-mv $MAVEN_VERSION-bin /opt/maven
+mv $MAVEN_VERSION /opt/maven
+rm -rf $MAVEN_VERSION-bin.tar.gz
 
-cp maven.sh /etc/profile.d/
-source /etc/profile.d/
+cp maven.sh /etc/profile.d/maven.sh
+
+echo "run: source /etc/profile.d/maven.sh to load mvn into the env"
