1fa7227bf2ed6c0e71b4d3bac17ac4be7da905f8
Valentin Uveges <valentin.uveges@gmail.com>
2017-08-26 21:23:05 +0300

    Update install.sh

--- guacamole/install.sh
+++ guacamole/install.sh
@@ -1,18 +1,18 @@
-//Prerequisites:
-//  - mysql
-//  - tomcat
-//  - 'Development Tools'
-//  - EPEL repo
+#Prerequisites:
+#  - mysql
+#  - tomcat
+#  - 'Development Tools'
+#  - EPEL repo
 
-//install ffmpeg ffmpeg-devel
+#install ffmpeg ffmpeg-devel
 rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
 rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
-yum install ffmpeg ffmpeg-devel -y
+yum install -y ffmpeg ffmpeg-devel
 
-//required deps
+#required deps
 yum install -y cairo-devel libjpeg-turbo-devel libjpeg-devel libpng-devel uuid-devel
 
-//optional deps
+#optional deps
 yum install -y freerdp-devel pango-devel libssh2-devel libtelnet-devel libvncserver-devel pulseaudio-libs-devel openssl-devel libvorbis-devel libwebp-devel
 
 wget "http://apache.org/dyn/closer.cgi?action=download&filename=incubator/guacamole/0.9.13-incubating/source/guacamole-server-0.9.13-incubating.tar.gz" -O guacamole-server-0.9.13-incubating.tar.gz
@@ -26,16 +26,16 @@ guacd
 cd ..
 rm -rf guacamole-server-0.9.13-incubating
 
-//this assumes a tomcat server installation located in /opt/tomcat
+#this assumes a tomcat server installation located in /opt/tomcat
 wget "http://apache.org/dyn/closer.cgi?action=download&filename=incubator/guacamole/0.9.13-incubating/binary/guacamole-0.9.13-incubating.war" -O guacamole-0.9.13-incubating.war
 mv guacamole-0.9.13-incubating.war /opt/tomcat/webapps/guacamole.war
 
-//GUACAMOLE_HOME
+#GUACAMOLE_HOME
 mkdir ~./.guacamole
 mkdir ~./.guacamole/extensions
 mkdir ~./.guacamole/lib
 
-//databse
+#databse
 mysql -u root -p
 CREATE DATABASE guacamole_db;
 CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'powerGuacamole_2017';
@@ -59,5 +59,5 @@ rm -rf mysql-connector-java-5.1.43.tar.gz
 mv mysql-connector-java-5.1.43/mysql-connector-java-5.1.43-bin.jar ~./.guacamole/lib/
 rm -rf mysql-connector-java-5.1.43
 
-// copy guacamole.properties in .guacamole
+#copy guacamole.properties in .guacamole
 cp ./guacamole.properties ~./.guacamole
