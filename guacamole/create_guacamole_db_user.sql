2cc843f4cb96a7b7b3843a1a9243577b8e4220a2
Valentin Uveges <valentin.uveges@gmail.com>
2017-08-27 11:23:32 +0300

    Create create_guacamole_db_user.sql

---
+++ guacamole/create_guacamole_db_user.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE guacamole_db;
+CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'powerGuacamole_2017';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
+FLUSH PRIVILEGES;
