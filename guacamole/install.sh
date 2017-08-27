4f83a5c56dde212ad8ef506868baf2c7c881300d
Valentin Uveges <valentin.uveges@gmail.com>
2017-08-27 21:03:06 +0300

    Update install.sh

--- guacamole/install.sh
+++ guacamole/install.sh
@@ -17,6 +17,7 @@ yum install -y freerdp-devel pango-devel libssh2-devel libtelnet-devel libvncser
 
 wget "http://apache.org/dyn/closer.cgi?action=download&filename=incubator/guacamole/0.9.13-incubating/source/guacamole-server-0.9.13-incubating.tar.gz" -O guacamole-server-0.9.13-incubating.tar.gz
 tar -xzf guacamole-server-0.9.13-incubating.tar.gz
+rm -rf guacamole-server-0.9.13-incubating.tar.gz
 cd guacamole-server-0.9.13-incubating
 ./configure
 make
@@ -36,28 +37,26 @@ mkdir ~/.guacamole/extensions
 mkdir ~/.guacamole/lib
 
 #databse
-mysql -u root -p
-CREATE DATABASE guacamole_db;
-CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'powerGuacamole_2017';
-GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
-FLUSH PRIVILEGES;
-quit
+echo "Connecting to mysql using root"
+mysql -u root -p < create_guacamole_db_user.sql
+
 
 wget "http://apache.org/dyn/closer.cgi?action=download&filename=incubator/guacamole/0.9.13-incubating/binary/guacamole-auth-jdbc-0.9.13-incubating.tar.gz" -O guacamole-auth-jdbc-0.9.13-incubating.tar.gz
 tar -xzf guacamole-auth-jdbc-0.9.13-incubating.tar.gz
 rm -rf guacamole-auth-jdbc-0.9.13-incubating.tar.gz
-cp guacamole-auth-jdbc-0.9.13-incubating/mysql/guacamole-auth-jdbc-mysql-0.9.13-incubating.jar ~./.guacamole/extensions/
+cp guacamole-auth-jdbc-0.9.13-incubating/mysql/guacamole-auth-jdbc-mysql-0.9.13-incubating.jar ~/.guacamole/extensions/
 
 cd guacamole-auth-jdbc-0.9.13-incubating/mysql
+echo "Connecting to mysql using root"
 cat schema/*.sql | mysql -u root -p guacamole_db
-cd
+cd ../..
 rm -rf guacamole-auth-jdbc-0.9.13-incubating
 
 wget "https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.43.tar.gz"
 tar -xzf mysql-connector-java-5.1.43.tar.gz
 rm -rf mysql-connector-java-5.1.43.tar.gz
-mv mysql-connector-java-5.1.43/mysql-connector-java-5.1.43-bin.jar ~./.guacamole/lib/
+mv mysql-connector-java-5.1.43/mysql-connector-java-5.1.43-bin.jar ~/.guacamole/lib/
 rm -rf mysql-connector-java-5.1.43
 
 #copy guacamole.properties in .guacamole
-cp ./guacamole.properties ~./.guacamole
+cp guacamole.properties ~/.guacamole
