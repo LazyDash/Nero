803fa4f890f59e9c65fd3256515174300a2af1a4
Valentin Uveges <valentin.uveges@gmail.com>
2018-03-21 12:58:33 +0100

    update tomcat version and url

--- tomcat/instal.sh
+++ tomcat/instal.sh
@@ -3,9 +3,9 @@
 #Prerequisites:
 #  - havedge
 
-TOMCAT_VERSION=apache-tomcat-9.0.0.M26
+TOMCAT_VERSION=apache-tomcat-9.0.6
 
-wget "http://mirror.evowise.com/apache/tomcat/tomcat-9/v9.0.0.M26/bin/$TOMCAT_VERSION.tar.gz"
+wget "http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.6/bin/$TOMCAT_VERSION.tar.gz"
 tar -xzf $TOMCAT_VERSION.tar.gz
 mv $TOMCAT_VERSION /opt/tomcat
 rm -rf $TOMCAT_VERSION.tar.gz
