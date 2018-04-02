78d03676077db881d5b1111af23c47608bbc82ec
Valentin Uveges <valentin.uveges@gmail.com>
2018-04-02 12:06:24 +0200

    downgrade java version from 10 to 9 because of maven compile issues

--- java/install.sh
+++ java/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #vars
-JAVA_JDK_URL=http://download.oracle.com/otn-pub/java/jdk/10+46/76eac37278c24557a3c4199677f19b62/jdk-10_linux-x64_bin.rpm
+JAVA_JDK_URL="http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_linux-x64_bin.rpm"
 
 #main
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "$JAVA_JDK_URL" -O java_install.rpm
