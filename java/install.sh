2add860bdc26f525cf8f19a094cc26cee013d9e2
Valentin Uveges <valentin.uveges@gmail.com>
2018-04-02 22:17:13 +0200

    downgrade from java 1.9 to java 1.8 because of spring boot

--- java/install.sh
+++ java/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #vars
-JAVA_JDK_URL="http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_linux-x64_bin.rpm"
+JAVA_JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-i586.rpm"
 
 #main
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "$JAVA_JDK_URL" -O java_install.rpm
