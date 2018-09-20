5544b1cc4c928dd4e26eb30ac1ca4bc38a8b963e
Valentin Uveges <valentin.uveges@gmail.com>
2018-09-20 12:33:18 +0000

    update

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #install nginx
-yum install nginx
+yum install -y nginx
 
 #enable and run nginx
 systemctl enable nginx
@@ -14,13 +14,15 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/ngi
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
 #add redirect from 80 to 443
-mkdir /etc/nginx/default.d
 echo "return 301 https://\$host\$request_uri/;" >> /etc/nginx/default.d/ssl-redirect.conf
+
+#add lazydash.comf
+cp ./ssl.conf /etc/nginx/conf.d
+
+#allow SELinux exception for reverse proxy
+setsebool -P httpd_can_network_connect true
 
 #enable changes and restart nginx
 nginx -t
 systemctl restart nginx
 
-#add lazydash.comf
-mkdir /etc/nginx/conf.d
-cp ./ssl.conf /etc/nginx/conf.d
--- system/install.sh
+++ system/install.sh
@@ -6,6 +6,9 @@ yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusi
 yum groupinstall -y 'Development Tools'
 yum install -y wget
 
+#install monospace font
+yum install -y gnu-free-mono-fonts
+
 #configure network for openvpn forward
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 systemctl restart network.service
