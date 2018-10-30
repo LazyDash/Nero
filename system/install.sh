b509b20799d16fab6fd43d3c7dd977b7e6508eb0
Valentin Uveges <valentin.uveges@gmail.com>
2018-10-30 19:22:41 +0200

    add iperf3 to system optional programs

--- system/install.sh
+++ system/install.sh
@@ -4,7 +4,6 @@
 yum install -y epel-release
 yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
 yum groupinstall -y 'Development Tools'
-yum install -y wget
 
-#install htop
-yum install -y htop
+#intall optional programs
+yum install -y wget iperf3 htop
